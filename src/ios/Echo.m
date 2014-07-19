adec28ea3494e7e23f96d8797fbfab3741b40bcf
Swati <atkominfotech@Atkoms-Mac-mini.local>
2014-07-19 15:58:08 +0530

    commited changes

---
+++ src/ios/Echo.m
@@ -0,0 +1,35 @@
+/********* Echo.m Cordova Plugin Implementation *******/
+
+#import "Echo.h"
+#import <Cordova/CDV.h>
+
+@implementation Echo
+
+// - (void)echo:(CDVInvokedUrlCommand*)command
+// {
+//     CDVPluginResult* pluginResult = nil;
+//     NSString* echo = [command.arguments objectAtIndex:0];
+
+//     if (echo != nil && [echo length] > 0) {
+//         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+//     } else {
+//         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+//     }
+
+//     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+// }
+
+
+- (void)echo:(CDVInvokedUrlCommand*)command
+{
+    // Check command.arguments here.
+    [self.commandDelegate runInBackground:^{
+        NSString* payload = nil;
+        // Some blocking logic...
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
+        // The sendPluginResult method is thread-safe.
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+@end
